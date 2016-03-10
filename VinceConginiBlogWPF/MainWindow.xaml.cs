f636d5cf34b061a7a7181709e8cac3638a2f7ab1
vcongini <vcongini@hotmail.com>
2016-03-10 09:00:07 -0800

    Initial example commit.

---
+++ VinceConginiBlogWPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+
+namespace ProgressBar
+    {
+    public partial class ProgressBarTask : Window
+        {
+        public ProgressBarTask ()
+            {
+            InitializeComponent ();
+            }
+
+        private void contentRendered (object sender, EventArgs e)
+            {
+            BackgroundWorker worker = new BackgroundWorker ();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += doWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+
+            worker.RunWorkerAsync ();
+            }
+
+        void doWork (object sender, DoWorkEventArgs e)
+            {
+            for (int i = 0; i < 100; i++)
+                {
+                (sender as BackgroundWorker).ReportProgress (i);
+                Thread.Sleep (100);
+                }
+            }
+
+        void worker_ProgressChanged (object sender, ProgressChangedEventArgs e)
+            {
+            pbStatus.Value = e.ProgressPercentage;
+            }
+        }
+    }
